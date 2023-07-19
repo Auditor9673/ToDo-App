93e5c70ef84b953394b214d5cd9e809b28e6c306
Auditor9673 <112822602+Auditor9673@users.noreply.github.com>
2023-07-19 06:08:24 +0530

    corrected

--- Modules/functions.py
+++ Modules/functions.py
@@ -1,9 +1,9 @@
-def get_todos(filepath = 'The ToDo List Project\Text Files\ToDo.txt'):
+def get_todos(filepath = 'Text Files\ToDo.txt'):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath = 'The ToDo List Project\Text Files\ToDo.txt'):
+def write_todos(todos_arg, filepath = 'Text Files\ToDo.txt'):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
